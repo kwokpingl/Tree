65142eb34d8c34e14d94d056b5f2fb67e610594d
Scott Riccardelli <s.riccardelli@gmail.com>
2015-07-30 20:19:33 -0400

    Initial Commit

---
+++ Tree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tree
+//
+//  Created by Scott on 7/30/15.
+//  Copyright © 2015 Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
